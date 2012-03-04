dee5cab8fc24d8044ab15ceefbafc7c3385aba96
Rahul Trikha <rahul.trikha@gmail.com>
2012-03-05 10:15:37 +1030

    Added Products

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,6 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Product.create!(name: "Settlers of Catan", price: 29.95)
+Product.create!(name: "Agricola", price: 45.99)
+Product.create!(name: "7 Wonders", price: 28.75)
+Product.create!(name: "Stone Age", price: 34.99)
+Product.create!(name: "Dominion", price: 29.99)
+Product.create!(name: "Goban", price: 175.95)
